e9d239f44534fa74cc6c971e6170f6b734aa71b6
Lurie <lurainekimmerle@gmail.com>
2023-08-22 08:38:27 +0000

    set pasted in challenge from Scrimba-first part done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import "./styles.css";
-import React from 'react'
+import React from "react"
 
+function App(){
+const [boxArray, setboxArray]= React.useState(boxes)
+console.log(boxArray)
+    
+const showBoxes = boxArray.map(box =>
+        <div key={box.id} 
+        className="boxes">
+        </div>
+        )
+        
+    return (
+        <main>
+            <h1>Boxes will go here</h1>
+            {showBoxes}
+        </main>
+    )
+    }
+export default App
 
-export default function App() {
-    const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (719) 555-1212",
-        email: "itsmyrealname@example.com",
-        isFavorite: false
-    })
+//contact Card Challenge
+// export default function App() {
+//     const [contact, setContact] = React.useState({
+//         firstName: "John",
+//         lastName: "Doe",
+//         phone: "+1 (719) 555-1212",
+//         email: "itsmyrealname@example.com",
+//         isFavorite: false
+//     })
     /**
      * Challenge: Use a ternary to determine which star image filename
      * should be used based on the `contact.isFavorite` property
@@ -20,40 +38,40 @@ export default function App() {
      * Then use the starIcon value to display the correct image
      */
     
-    let starIcon = contact.isFavorite ? "./images/star-filled.png" : "./images/star-empty.png";
+//     let starIcon = contact.isFavorite ? "./images/star-filled.png" : "./images/star-empty.png";
     
-    console.log(contact.isFavorite)
-    console.log(starIcon)
+//     console.log(contact.isFavorite)
+//     console.log(starIcon)
     
-    function toggleFavorite() {
-        setContact( prevContact => {
-           return {...prevContact,
-            isFavorite: !preContact.isFavorite
-        }
-        })
-    }
+//     function toggleFavorite() {
+//         setContact( prevContact => {
+//            return {...prevContact,
+//             isFavorite: !preContact.isFavorite
+//         }
+//         })
+//     }
     
-    return (
-        <main>
-            <article className="card">
-                <img src="./images/user.png" className="card--image" />
-                <div className="card--info">
-                    <img 
-                        src={starIcon}
-                        className="card--favorite"
-                        onClick={toggleFavorite}
-                    />
-                    <h2 className="card--name">
-                        {contact.firstName} {contact.lastName}
-                    </h2>
-                    <p className="card--contact">{contact.phone}</p>
-                    <p className="card--contact">{contact.email}</p>
-                </div>
+//     return (
+//         <main>
+//             <article className="card">
+//                 <img src="./images/user.png" className="card--image" />
+//                 <div className="card--info">
+//                     <img 
+//                         src={starIcon}
+//                         className="card--favorite"
+//                         onClick={toggleFavorite}
+//                     />
+//                     <h2 className="card--name">
+//                         {contact.firstName} {contact.lastName}
+//                     </h2>
+//                     <p className="card--contact">{contact.phone}</p>
+//                     <p className="card--contact">{contact.email}</p>
+//                 </div>
                 
-            </article>
-        </main>
-    )
-}
+//             </article>
+//         </main>
+//     )
+// }
 
 
 
